61c0474fcbdf4d33d6efca401caba2cc7202ff4d
Madhav Murthy <madhavmurthy93@gmail.com>
2024-11-26 19:36:53 +0530

    Implement a pizza ordering service using react router

---
+++ 16-fast-react-pizza/src/features/menu/Menu.jsx
@@ -0,0 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+function Menu() {
+  const menu = useLoaderData();
+  return (
+    <div>
+      <h1>Menu</h1>
+      <ul>
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
