b898e552967343f814a9f997c463a070476350cf
Madhav Murthy <madhavmurthy93@gmail.com>
2024-12-04 18:28:07 +0530

    Make a resuable table and use it for cabin table

--- 17-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,25 +1,13 @@
-import { useState } from "react";
 import { HiOutlinePencil, HiOutlineTrash, HiSquare2Stack } from "react-icons/hi2";
 import styled from "styled-components";
 import Button from "../../ui/Button";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Modal from "../../ui/Modal";
+import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useCreateCabin } from "./useCreateCabin";
 import { useDeleteCabin } from "./useDeleteCabin";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1.5fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
 
 const Img = styled.img`
   display: block;
@@ -58,8 +46,6 @@ const Buttons = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const [showForm, setShowForm] = useState(false);
-
   const { id: cabinId, name, maxCapacity, regularPrice, discount, description, image } = cabin;
 
   const { isDeleting, deleteCabin } = useDeleteCabin();
@@ -78,7 +64,7 @@ function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row role="row">
         <Img src={image} />
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity} guests</div>
@@ -114,7 +100,7 @@ function CabinRow({ cabin }) {
             </Modal.Window>
           </Modal>
         </Buttons>
-      </TableRow>
+      </Table.Row>
     </>
   );
 }
